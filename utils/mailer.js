fa1c30cd457e71ad4d7a70e8ae98f6cef06a1709
megaton20 <128734956+megaton20@users.noreply.github.com>
2025-11-03 21:21:22 +0100

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,10 +1,10 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-      service: 'gmail',
+
       host: 'smtp-relay.brevo.com',
-       port: 465,
-        secure: true,
+       port: 587,
+        secure: false,
       auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD
